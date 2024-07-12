3d7fa06420fd9b1e579bd67b5e9caec7d22cdb9a
Omar abu assaf <xxluciferxx123@gmail.com>
2024-07-12 23:28:51 +0300

    plo

--- pics.py
+++ pics.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 def app():
-    st.write("^^^^^^^ there is menu up here ^^^^^^^^^")
+    st.write("^^^^^^^ there is a menu up here ^^^^^^^^^")
     st.title("Where it all happend")
-    st.write("""
+    st.write("""  
              It all started in room 201 in the statistics class, the first time I saw you.
              I remember seeing you entering the room and you just left me lost in my own thought
              I was like "ouuuffff, who is this", that was the moment you caught my attention,
