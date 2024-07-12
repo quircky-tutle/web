c04e618823de729d40acd16283827626d9ff79d7
Omar abu assaf <xxluciferxx123@gmail.com>
2024-07-12 22:37:22 +0300

    fofo

--- jiji.py
+++ jiji.py
@@ -1,9 +1,11 @@
 import streamlit as st
 
 def app():
-    st.header("jiji and joud wanted to celebrate this occasion with us and requested me to place them somewhere in the website, so here they are 😁😁😁")
-    col1 ,col2=st.columns(2)
+    st.header("jiji, joud and farah wanted to celebrate this occasion with us and requested me to place them somewhere in the website, so here they are 😁😁😁")
+    col1 ,col2,col3=st.columns(3)
     with col1:
-        st.image("./Images/jiji.JPEG",width=300)
+        st.image("./Images/jiji.JPEG",width=200)
     with col2:
-        st.image("./Images/joud.jpg",width=300)
+        st.image("./Images/joud.jpg",width=200)
+    with col3:
+        st.iamge("./Images/farah.jpg",width=200)
